654a47f1cd80b9fd974876644e84636685c06a9b
Gaurav <gauravsomani52750@gmail.com>
2023-01-25 09:57:05 +0530

    added django-cors-heeaders package

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd Party
     'rest_framework',
+    'corsheaders',
 
     # Local
     'todos',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,4 +135,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ]
+}
+
+
+CORS_ORIGIN_WHITELIST = {
+    'http;//localhost:3000',
+    'http;//localhost:8000',
 }
